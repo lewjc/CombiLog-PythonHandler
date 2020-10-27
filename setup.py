0aa87daec760d2d9cf3000e9e20e5766d5fc40ff
lewjc <lewisjcummins@hotmail.co.uk>
2020-10-27 12:38:24 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="combilog_handler",  # Replace with your own username
-    version="1.0.7",
+    name="combilog-handler",  # Replace with your own username
+    version="1.0.10",
     author="Lewis Cummins",
     author_email="lewisjcummins@hotmail.co.uk",
     description="Interfaces with inbuilt python logger to send logs to combilog aggregator.",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lewjc/combilog-pythonhandler",
     packages=setuptools.find_packages(),
+    install_requires=["websocket_client", "asyncio"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
